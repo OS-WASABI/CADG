f07847d056f15650e711450d02c1b981aa6a24be
Ross <rosarc@gmail.com>
2019-04-04 20:07:58 -0500

    Created starting point for CMAC data object content filling
    - Provided notes for future developers to continue development.

--- rest-server/src/model/cmac/cmac_alert.hpp
+++ rest-server/src/model/cmac/cmac_alert.hpp
@@ -3,17 +3,15 @@
 #include <vector>
 #include <optional>
 #include "cmac_alert_info.hpp"
+#include "../../../../soap-server/src/gSOAPFiles/soapStub.h"
 
 #ifndef CADG_REST_SERVER_CMAC_ALERT_HPP
 #define CADG_REST_SERVER_CMAC_ALERT_HPP
 
 namespace cmac {
     struct CMAC_alert {
-        // TODO(Ross): Determine how to get the protocol version based on either CAP message or other source.
         std::optional<std::string> cmac_protocol_version;
-        // TODO(Ross): Determine how to get gateway IP or URI.
         std::optional<std::string> cmac_sending_gateway_id;
-        // TODO(Ross): Determine how to identify CMSP-initiated value, when applicable.
         std::optional<std::string> cmac_message_number;
         std::optional<std::string> cmac_referenced_message_number;
         std::optional<std::string> cmac_referenced_message_cap_identifier;
@@ -31,6 +29,27 @@ namespace cmac {
         std::optional<cmac::CMAC_alert_info> cmac_alert_info;
         std::optional<std::string> cmac_digital_signature;
     };
+
+    // NOTE: This method currently DOES NOT WORK. It is here to serve as a starting point
+    // for future developers to refine, build upon, and complete. Its intent is to take an external
+    // object containing the SOAP CAP input of an incoming message and use it to create a CMAC
+    // data object based on the logic outlined in the ATIS-0700037 PDF. This data object will then
+    // be passed to the cmac_converter.cpp convert method found in the soap/cmac-conversion directory.
+    // Finally, this method should make calls to similar methods found in the cmac_alert_info,
+    // cmac_alert_area, cmac_alert_text hpp files as necessary in order to complete the cmac_alert object.
+    // Those methods currently do not exist.
+    void fillAlertObject(_ns4__alert soap_input, cmac::CMAC_alert new_alert) {
+        // TODO(Ross): Determine how to get the protocol version based on either CAP message or other source.
+        new_alert.cmac_protocol_version = "Temp value";
+        // TODO(Ross): Determine how to get gateway IP or URI.
+        new_alert.cmac_sending_gateway_id = "Temp value";
+        // TODO(Ross): Determine how to identify CMSP-initiated value, when applicable.
+        new_alert.cmac_message_number = soap_input.identifier; // Likely temporary value
+        new_alert.cmac_referenced_message_number = soap_input.identifier; // Likely temporary value
+        new_alert.cmac_referenced_message_cap_identifier = soap_input.identifier; // Maybe temporary value?
+        // TODO(Ross): Determine value
+        new_alert.cmac_special_handling = "Temp value";
+    }
 }
 
 #endif //CADG_REST_SERVER_CMAC_ALERT_HPP
